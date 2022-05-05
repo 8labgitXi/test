08f37f9ed29c24bb44bea1abfdddd2685c38bc4e
8labgitXi <xixiaoyu@8lab.cn>
2022-05-05 10:41:35 +0800

    修改配置

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@File        : __init__.py.py
-@Author      : liuda
-@Time        : 2022/2/6 12:39
-@Description :
-"""
--- utils/drf/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@File        : __init__.py.py
-@Author      : yu wen yang
-@Time        : 2022/1/24 9:11 上午
-@Description :
-"""
